ac72edc6a5a351f3f644a8cc8b22eb1edc99cd89
papey08 <popov.m4tvei@yandex.ru>
2023-07-30 11:46:49 +0300

    Add graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,9 +5,14 @@ import (
 	"console-chat/internal/ports/ginserver"
 	"console-chat/internal/ports/wsserver"
 	userrepo "console-chat/internal/repo/user_repo"
+	"context"
 	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Pallinder/go-randomdata"
 	"github.com/spf13/viper"
@@ -29,7 +34,26 @@ func main() {
 	ws := wsserver.NewWsServer(tokenKey)
 	app := app.New(userrepo.New())
 	server := ginserver.NewHTTPServer(host, port, ws, app, tokenKey)
-	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-		log.Fatalf("can't listen and serve server: %s", err.Error())
+
+	// preparing graceful shutdown
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGINT)
+
+	go func() {
+		log.Println("Starting http server")
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("can't listen and serve server: %s", err.Error())
+		}
+	}()
+
+	<-osSignals
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server graceful shutdown failed:", err.Error())
 	}
+	log.Println("Server was gracefully stopped")
 }
